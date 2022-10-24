62c23e2327def5342b78aeccebfc54b5543191f4
sam-corbett <sam-corbett@users.noreply.github.com>
2022-10-24 22:17:52 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // Created on: Sep 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * This function calculates both work hours and hourly rate.
  */
@@ -12,14 +12,16 @@
 function calculate() {
   // input
   const TAX_RATE = 0.18
-  const hours = parseInt(document.getElementById('work-hours').value)
-  const rate = parseInt(document.getElementById('hourly-rate').value)
+  const hours = parseInt(document.getElementById("work-hours").value)
+  const rate = parseInt(document.getElementById("hourly-rate").value)
 
   // process
-  const pay = (hours * rate) * (1.00 - TAX_RATE)
-  const taxes = (hours * rate) * TAX_RATE
+  const pay = hours * rate * (1.0 - TAX_RATE)
+  const taxes = hours * rate * TAX_RATE
 
   // output
-  document.getElementById('pay').innerHTML = 'Your Payment Is Around ' + pay + '.'
-  document.getElementById('taxes').innerHTML = 'Your Taxes Are Around ' + taxes + '.'
+  document.getElementById("pay").innerHTML =
+    "Your Payment Is Around " + pay + "."
+  document.getElementById("taxes").innerHTML =
+    "Your Taxes Are Around " + taxes + "."
 }
