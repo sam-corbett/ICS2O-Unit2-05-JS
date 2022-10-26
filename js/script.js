fce02e065a31213a929a6bda3167daa52b8ee763
sam-corbett <113055935+sam-corbett@users.noreply.github.com>
2022-10-26 10:32:38 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Sam Corbett All rights reserved
 //
 // Created by: Sam Corbett
-// Created on: O 2022
+// Created on: Oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
